8963c0623690fa8d3240739c274f0eaf8df44de8
rajib <rajib66k@gmail.com>
2026-07-07 15:46:32 +0530

    feat: add Chainlink oracle stale price validation

---
+++ src/libraries/OracleLib.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.35;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+/**
+ * @title OracleLib
+ * @author Rajib Kuamar Pradhan
+ * @notice This library is used to check the Chainlink Oracle for stale data.
+ * If a price is stale, functions will revert, and render the lending-protocol unusable - this is by design.
+ * We want the lending-protocol to freeze if prices become stale.
+ *
+ * So if the Chainlink network explodes and you have a lot of money locked in the protocol... too bad.
+ */
+
+library OracleLib {
+    error OracleLib__StalePrice();
+
+    uint256 private constant TIMEOUT = 3 hours;
+
+    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed)
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            priceFeed.latestRoundData();
+
+        uint256 secondsSince = block.timestamp - updatedAt;
+
+        if (answer <= 0 || updatedAt == 0 || answeredInRound < roundId || secondsSince > TIMEOUT) {
+            revert OracleLib__StalePrice();
+        }
+
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    }
+
+    function getTimeout() public pure returns (uint256) {
+        return TIMEOUT;
+    }
+}
