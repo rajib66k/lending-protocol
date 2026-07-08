1d26789ea0edd5da29f4c3dd9b2daace208cde83
rajib <rajib66k@gmail.com>
2026-07-08 22:58:38 +0530

    test: add unit tests for OracleLib library

---
+++ test/libraries/OracleLibTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.35;
+
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
+import {Test} from "forge-std/Test.sol";
+import {OracleLib, AggregatorV3Interface} from "../../src/libraries/OracleLib.sol";
+
+contract OracleLibTest is Test {
+    using OracleLib for AggregatorV3Interface;
+
+    MockV3Aggregator public aggregator;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITAL_PRICE = 2000 ether;
+
+    function setUp() public {
+        aggregator = new MockV3Aggregator(DECIMALS, INITAL_PRICE);
+    }
+
+    function testGetTimeout() public pure {
+        uint256 expectedTimeout = 3 hours;
+        assertEq(OracleLib.getTimeout(), expectedTimeout);
+    }
+
+    function testPriceRevertsOnStaleCheck() public {
+        vm.warp(block.timestamp + 3 hours + 1 seconds);
+        vm.roll(block.number + 1);
+
+        vm.expectRevert(OracleLib.OracleLib__StalePrice.selector);
+        AggregatorV3Interface(address(aggregator)).staleCheckLatestRoundData();
+    }
+
+    function testPriceRevertsOnBadAnsweredInRound() public {
+        uint80 _roundId = 0;
+        int256 _answer = 0;
+        uint256 _timestamp = 0;
+        uint256 _startedAt = 0;
+        aggregator.updateRoundData(_roundId, _answer, _timestamp, _startedAt);
+
+        vm.expectRevert(OracleLib.OracleLib__StalePrice.selector);
+        AggregatorV3Interface(address(aggregator)).staleCheckLatestRoundData();
+    }
+}
