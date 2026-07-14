6f95c691c0deaad493455e19b7e7d63592f499a7
rajib <rajib66k@gmail.com>
2026-07-14 20:04:59 +0530

    feat: add price conversion logic

--- src/protocol/Pool.sol
+++ src/protocol/Pool.sol
@@ -6,12 +6,14 @@ import {ReserveLogic} from "../libraries/ReserveLogic.sol";
 import {ValidationLogic} from "../libraries/ValidationLogic.sol";
 import {Math} from "../libraries/Math.sol";
 import {IPool} from "../interfaces/IPool.sol";
+import {OracleLib} from "../libraries/OracleLib.sol";
 import {ILiquidityToken} from "../interfaces/ILiquidityToken.sol";
 import {IDebtToken} from "../interfaces/IDebtToken.sol";
 import {UserConfiguration} from "../configuration/UserConfiguration.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title Pool
@@ -23,6 +25,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 abstract contract Pool is IPool, ReentrancyGuard, Ownable {
     using ReserveLogic for DataTypes.ReserveData;
     using ValidationLogic for DataTypes.ReserveCache;
+    using OracleLib for AggregatorV3Interface;
     using Math for uint256;
     using SafeERC20 for IERC20;
 
@@ -68,5 +71,55 @@ abstract contract Pool is IPool, ReentrancyGuard, Ownable {
         IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
         ILiquidityToken(reserveCache.liquidityTokenAddress).mint(onBehalfOf, scaledAmount);
         emit Supply(asset, msg.sender, onBehalfOf, amount);
+    }
+
+    /**
+     * @notice Converts asset token amount into the equivalent USD-denominated amount
+     * @param asset The ERC20 token address
+     * @param amount The asset amount (18 decimals precision)
+     * @return The equivalent amount of USD-denominated amount
+     * @dev Uses Chainlink price feeds for token/USD conversion & OracleLib for stale check
+     */
+    function _usdValue(address asset, uint256 amount) internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(feeds[asset].priceFeedAddress);
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
+
+        uint256 tokenDecimals = feeds[asset].tokenDecimals;
+        uint256 feedDecimals = feeds[asset].feedDecimals;
+
+        if (feedDecimals < 18) {
+            // casting to 'uint256' is safe because OracleLib guarantees a non-negative answer
+            // forge-lint: disable-next-line(unsafe-typecast)
+            return (amount * (uint256(price)) * 10 ** (18 - feedDecimals) / 10 ** tokenDecimals);
+        } else {
+            // casting to 'uint256' is safe because OracleLib guarantees a non-negative answer
+            // forge-lint: disable-next-line(unsafe-typecast)
+            return (amount * uint256(price)) / (10 ** tokenDecimals * 10 ** (feedDecimals - 18));
+        }
+    }
+
+    /**
+     * @notice Converts a USD-denominated amount into the equivalent payment token amount
+     * @param asset The ERC20 payment token address
+     * @param usdAmount The USD amount (18 decimals precision)
+     * @return The equivalent amount of payment tokens required
+     * @dev Uses Chainlink price feeds for token/USD conversion & OracleLib for stale check
+     */
+    function _tokenAmountFromUsd(address asset, uint256 usdAmount) internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(feeds[asset].priceFeedAddress);
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
+
+        uint256 tokenDecimals = feeds[asset].tokenDecimals;
+        uint256 feedDecimals = feeds[asset].feedDecimals;
+
+        if (feedDecimals < 18) {
+            // casting to 'uint256' is safe because OracleLib guarantees a non-negative answer
+            // forge-lint: disable-next-line(unsafe-typecast)
+            return (usdAmount * 10 ** tokenDecimals) / (uint256(price) * 10 ** (18 - feedDecimals));
+        } else {
+            // casting to 'uint256' is safe because OracleLib guarantees a non-negative answer
+            // forge-lint: disable-next-line(unsafe-typecast)
+            return (usdAmount * 10 ** tokenDecimals * 10 ** (feedDecimals - 18)) / (uint256(price));
+        }
     }
 }
